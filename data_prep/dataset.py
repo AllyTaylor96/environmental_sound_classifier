7e6ee07a4525090bddfb90f0a77d07e822d293d3
AllyTaylor96 <allytaylor@hotmail.co.uk>
2022-12-17 20:57:01 +0000

    Implemented base pytorch dataset

--- data_prep/dataset.py
+++ data_prep/dataset.py
@@ -1,10 +1,17 @@
 import torch
+import json
+import pickle
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
+with open('../config.json', 'r') as f:
+    config = json.load(f)
 
-def label_race (row):
-    return row.replace(
+def get_pickle_from_name(name, processed_path):
+    name_pickled = name.replace('.wav', '.pkl')
+    path = processed_path + '/' + name_pickled
+    return path
+
 
 class Esc50Dataset(Dataset):
     def __init__(self, csv_file, processed_dir):
@@ -14,9 +21,20 @@ class Esc50Dataset(Dataset):
             processed_dir (string): Path to directory with processed pickled spectrograms.
         """
         self.annotations = pd.read_csv(csv_file)
-        print(self.annotations)
+        self.annotations['picklepath'] = self.annotations.apply(lambda x: get_pickle_from_name(x['filename'], processed_dir), axis=1)
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __get_item__(self, idx):
+
+        feature_info = self.annotations.iloc[idx]
+        with open(feature_info.loc['picklepath'], 'rb') as f:
+            spec = pickle.load(f)
+        feature = {'path': feature_info.loc['picklepath'],
+                   'data': spec,
+                   'class': feature_info.loc['fold'],
+                   'subclass': feature_info.loc['category']}
+        return feature
 
 
-csv_file = '/home/ally_taylor/code/environmental_sound_classifier/data/labels/esc50.csv'
-processed_dir = '/home/ally_taylor/code/environmental_sound_classifier/data/processed'
-data = Esc50Dataset(csv_file, processed_dir)
